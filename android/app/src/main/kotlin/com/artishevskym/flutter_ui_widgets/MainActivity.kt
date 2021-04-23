6d31ca7eee01a819dd68cc22a73c5cb914718b20
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-23 21:50:04 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/artishevskym/flutter_ui_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.artishevskym.flutter_ui_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter UI Widgets"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(32.0),
+        padding: EdgeInsets.all(16.0),
+        width: 200,
+        height: 200,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16.0),
+          border: Border.all(width: 2, color: Colors.red),
+          color: Colors.green,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              color: Colors.blue,
+              child: Text(
+                "1",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 32),
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              child: Text(
+                "2",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 32),
+              ),
+            ),
+            Container(
+              color: Colors.blue,
+              child: Text(
+                "3",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 32),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print("Clicked");
+        },
+      ),
+    );
+  }
+}
