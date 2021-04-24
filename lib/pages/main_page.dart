bd0b95a9f24977d0b35a5c572e5adcbe140b32ca
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-24 18:13:44 +0100

    Build a card widget by combining different layout widgets and learn how to use the Stack widget to lay widgets on top of each other

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_widgets/models/article.dart';
+import 'package:flutter_ui_widgets/widgets/article_card.dart';
 
 class MainPage extends StatelessWidget {
   @override
@@ -7,46 +9,12 @@ class MainPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Flutter UI Widgets"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(32.0),
-        padding: EdgeInsets.all(16.0),
-        width: 200,
-        height: 200,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16.0),
-          border: Border.all(width: 2, color: Colors.red),
-          color: Colors.green,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          // crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              color: Colors.blue,
-              child: Text(
-                "1",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 32),
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              child: Text(
-                "2",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 32),
-              ),
-            ),
-            Container(
-              color: Colors.blue,
-              child: Text(
-                "3",
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 32),
-              ),
-            ),
-          ],
-        ),
+      body: Column(
+        children: <Widget>[
+          ArticleCard(
+            article: articles[0],
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
