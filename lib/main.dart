32c6b35e4471cecf05d584d939971d208020a0e8
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-24 22:32:23 +0100

    Custom light theme

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,19 @@ import 'package:flutter_ui_widgets/pages/main_page.dart';
 
 void main() => runApp(MyApp());
 
+final lightTheme = ThemeData(
+  primaryColor: Colors.red,
+  accentColor: Colors.green,
+);
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter UI Widget App",
-      theme: ThemeData.light(),
+      theme: lightTheme,
       darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.dark,
+      // themeMode: ThemeMode.dark,
       home: MainPage(),
     );
   }
