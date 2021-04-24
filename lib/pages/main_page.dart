d2978505dea0bf36fd30dc4937472a54529c988a
Martin Artishevsky <artishevsky.m@gmail.com>
2021-04-24 19:46:28 +0100

    Work with BottomNavigationBar

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -2,7 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ui_widgets/models/article.dart';
 import 'package:flutter_ui_widgets/widgets/article_card.dart';
 
-class MainPage extends StatelessWidget {
+import 'home_page.dart';
+import 'inbox_page.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final List<Widget> _pages = [
+    HomePage(),
+    InboxPage(),
+  ];
+
+  int _selectedIndex = 0;
+
+  _onTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,17 +33,29 @@ class MainPage extends StatelessWidget {
           width: 180,
         ),
       ),
-      body: ListView.builder(
-        itemCount: articles.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ArticleCard(article: articles[index]);
-        },
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: <Widget>[..._pages],
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
         onPressed: () {
           print("Clicked");
         },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onTapped,
+        items: [
+          BottomNavigationBarItem(
+            label: "Home",
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            label: "Inbox",
+            icon: Icon(Icons.mail),
+          ),
+        ],
       ),
     );
   }
